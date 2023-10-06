312c8f32d4aa780d03a53f7d79592ace1d03b86e
Your Azevedo <gabrieldma27@gmail.com>
2023-10-06 17:17:31 -0300

    blog adicionado novamente

--- data/headerNavLinks.js
+++ data/headerNavLinks.js
@@ -1,7 +1,7 @@
 const headerNavLinks = [
   { href: '/', title: 'Home' },
   { href: '/#nossa-missao', title: 'Nossa Missão' },
-  // { href: '/blog', title: 'Blog' },
+  { href: '/blog', title: 'Blog' },
   // { href: '/tags', title: 'Tags' },
   { href: '/#areas-atuacao', title: 'Áreas de atuação' },
   { href: '/#contact', title: 'Contato' },
